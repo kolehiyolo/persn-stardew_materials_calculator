c3f0631fc453dfa09baad6ca64d8449c213c514b
Tristan Sean Paul Perfecto Cinco <tristanseanpaul.cinco@gmail.com>
2025-06-30 03:06:50 +0800

    reinit to Vite + React + TypeScript + SASS
    reinit to Vite + React + TypeScript + SASS
    1. I needed to update file types to tsx and GPT recommended that I re-initialize to use the Vite + React + TypeScript template
    2. did so, applied changes from last commit to new files

--- COMMIT.md
+++ COMMIT.md
@@ -1,6 +1,3 @@
-init types, scss, FEATURE-CREEP
-1. some more DX prep before actual code crunching
-2. prepared a list of MVP todos as well as Figma MVP screens
-3. prepared SCSS styling, just fetching my globals and variables from persn-adulting_101_v2
-4. prepared a rudimentary form of the 2 types we'll be using thru the app, Item and Material
-5. did some quick changes to App.jsx just so it's
+reinit to Vite + React + TypeScript + SASS
+1. I needed to update file types to tsx and GPT recommended that I re-initialize to use the Vite + React + TypeScript template
+2. did so, applied changes from last commit to new files
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
 // * Dependencies
-import { useState } from 'react'
+import { useState } from 'react';
 
 // * Other Components
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
 // * Other Imports
 import './styles/App.scss';
 
 // * Component
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,12 +28,12 @@ export default function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
+  );
+};
